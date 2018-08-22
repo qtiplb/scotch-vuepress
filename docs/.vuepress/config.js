d9a0250dcfc1f7ec5ed89c6dfa8eb979ba0fe634
qtiplb <31708522+qtiplb@users.noreply.github.com>
2018-08-22 11:33:08 -0400

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,6 +22,21 @@ module.exports = {
                   '/guide/api'
               ]
             }
-          ]
+          ],
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'qtiplb/scotch-vuepress',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    repoLabel: 'Contribute!',
+
+    // Optional options for generating "Edit this page" link
+    // if your docs are not at the root of the repo:
+    docsDir: 'docs',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'dev',
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Help us improve this page!'
     }
 }
