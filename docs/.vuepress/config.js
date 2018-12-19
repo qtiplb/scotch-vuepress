bc2b587ec89700dc86e274411e9f372faadef1a7
qtiplb <31708522+qtiplb@users.noreply.github.com>
2018-12-18 21:53:19 -0500

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,6 @@ module.exports = {
             title: 'Presentation',
             collapsable: false,
             children: [
-             '',      /* /bar/ */
                 '/presentation/presentation'
             ]
           },
